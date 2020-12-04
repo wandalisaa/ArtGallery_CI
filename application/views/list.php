a46fb347d8b78edbf8eb4317ec8a26136edb2774
Wanda-Khalishah <wndkhlish@gmail.com>
2020-12-04 20:14:45 +0700

    db

--- application/views/list.php
+++
@@ -1,8 +0,0 @@
-
-	<!-- Blog Page -->
-	<div class="blog__single__page pb-5">
-		<article class="blog__article pt-2 ">
-			<blockquote class="shadow bg-white p-3 mb-5" style="text-align: -webkit-center;">
-               
-			</blockquote>
-		</div>
